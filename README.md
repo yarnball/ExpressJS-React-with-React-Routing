d31f44c155682b6f5a2246d56deb7bdfa6e28897
Yarnball <yconomos@gmail.com>
2016-08-17 16:05:24 +1000

    Added MongoDB server

---
+++ README.md
@@ -0,0 +1,13 @@
+# React-router & ExpressJS
+
+An adaption from the great React-Router tutorials.
+
+# To run
+
+[*] npm install
+
+# To run
+
+[*] NODE_ENV=production npm start
+OR
+[*] npm start
---
+++ server.js
@@ -0,0 +1,43 @@
+console.log('May Node be with you')
+
+const express = require('express');
+const app = express();
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+var db
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+app.set('view engine', 'ejs')
+
+var ejs = require('ejs');
+ejs.open = '{{';
+ejs.close = '}}';
+
+MongoClient.connect('mongodb://***:***@***.mlab.com:***/gigst-db', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(8080, () => {
+    console.log('listening on 8080')
+  })
+})
+
+app.post('/actionhtmlref', (req, res) => {
+  db.collection('db_area').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
+
+app.get('/', (req, res) => {
+  db.collection('db_area').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    // renders index.ejs
+    res.render('index.ejs', {db_areas: result})
+  })
+})
